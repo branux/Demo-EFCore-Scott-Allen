1ab7137c868e97d2b42fa461102a26179fab3063
fabiano nalin <fabiano.nalin@gmail.com>
2016-10-27 13:23:55 -0200

    Add demo

---
+++ DemoCore.Data/Model/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DemoCore.Data.Model
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+}
---
+++ DemoCore.Data/Model/BloggingContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoCore.Data.Model
+{
+    public class BloggingContext : DbContext
+    {
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.ConsoleApp.NewDb;Trusted_Connection=True;");
+        }
+    }
+}
